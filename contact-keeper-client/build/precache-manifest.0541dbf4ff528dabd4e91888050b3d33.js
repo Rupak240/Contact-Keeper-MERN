ad379d36493b80887d6290aeaa67cc83611942d3
RupaK <rupakdey1998@gmail.com>
2020-08-28 14:46:58 +0530

    update1

---
+++ contact-keeper-client/build/precache-manifest.0541dbf4ff528dabd4e91888050b3d33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ce3be0b4e63a20dd082eb5afc97ad0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f762c27719e7c03e21a8",
+    "url": "/static/css/main.705d46d5.chunk.css"
+  },
+  {
+    "revision": "d36453a9b9c6e37cd36b",
+    "url": "/static/js/2.7442d7e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7442d7e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f762c27719e7c03e21a8",
+    "url": "/static/js/main.e380e9a7.chunk.js"
+  },
+  {
+    "revision": "2bca4cd739e097676d83",
+    "url": "/static/js/runtime-main.144ed0dc.js"
+  }
+]);
